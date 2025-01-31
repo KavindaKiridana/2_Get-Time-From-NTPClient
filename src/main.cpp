21c203aed5ccc691d1a4521f705da1b41f6e4f2b
KavindaKiridana <Kavindakiridena@gmail.com>
2025-01-31 17:03:31 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include <WiFiUdp.h>
 
 // Replace with your network credentials
-const char *ssid = "YOUR-WIFI-CONNECTION-NAME";
-const char *password = "WIFI-CONNECTION-PASSWORD";
+const char *ssid = "cracked 0027";
+const char *password = "5FdcD9D3";
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -43,7 +43,7 @@ void loop()
   // Get current time components
   int currentHour = timeClient.getHours();
   int currentMinute = timeClient.getMinutes();
-  int currentSecond = timeClient.getSeconds();
+  // int currentSecond = timeClient.getSeconds(); (if u want seconds value then uncomment this line)
 
   // Calculate date and month from epoch time
   time_t rawTime = timeClient.getEpochTime();
@@ -52,10 +52,11 @@ void loop()
   int currentMonth = timeInfo->tm_mon + 1;    // Extract month (tm_mon is 0-based)
   int currentYear = timeInfo->tm_year + 1900; // Extract year (tm_year is years since 1900)
 
-  // Combine into a single string variable
-  char dateTime[20]; // Ensure sufficient size
-  snprintf(dateTime, sizeof(dateTime), "%02d:%02d:%02d %02d:%02d:%04d",
-           currentHour, currentMinute, currentSecond, currentDay, currentMonth, currentYear);
+  String dateTime = String(currentHour) + ":" +
+                    String(currentMinute) + " " +
+                    String(currentDay) + "-" +
+                    String(currentMonth) + "-" +
+                    String(currentYear);
 
   // Print the dateTime string
   Serial.println(dateTime);
